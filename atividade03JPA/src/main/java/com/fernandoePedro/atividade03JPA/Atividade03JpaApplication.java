a2c484c533fef1bd0450d115e34bdac636063088
FabriciuSs <ph264915@gmail.com>
2021-12-13 16:27:06 -0300

    Upload de arquivos já prontos

---
+++ atividade03JPA/src/main/java/com/fernandoePedro/atividade03JPA/Atividade03JpaApplication.java
@@ -0,0 +1,15 @@
+package com.fernandoePedro.atividade03JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade03JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade03JpaApplication.class, args);
+
+		System.out.println("Teste");
+	}
+
+}
